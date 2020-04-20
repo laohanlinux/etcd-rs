5310dab1b5b16d51d09ebfbf1ce0b202298d501b
Rg <0x80@Rgs-MacBook-Pro.lan>
2020-04-21 01:39:49 +0800

    fix compile

---
+++ src/lease/lease_queue.rs
@@ -0,0 +1,12 @@
+
+// LeaseWithTime contains lease object with a time
+// For the leassor's lease heap, time identifies the lease expiration time.
+// For the leassor's lease checkpoint heap, the time identifies the next the lease checkpoint time.
+pub struct LeaseWithTime {
+  
+}
+
+
+// 
+pub struct LeaseExpiredNotifer {
+}
---
+++ src/lease/mod.rs
@@ -0,0 +1,3 @@
+pub mod lease_queue;
+
+pub const MAX_LEASE_TTL: u64 = 9000000000;
