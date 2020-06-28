62e23b11746d0f923ee4643771f906172fd7d38c
Rg <daimaldd@gmail.com>
2020-06-29 00:45:40 +0800

    :fire: add unstable test

--- src/raft/unstable.rs
+++ src/raft/unstable.rs
@@ -17,7 +17,7 @@ use crate::raft::raftpb::raft::{Snapshot, Entry};
 // unstable.entries[i] has raft log position i+unstable.offset.
 // Note that unstable.offset may be less than highest log
 // position in storage; this means that the next write to storage
-// might nned to truncate the log before persisting unstable.entries.
+// might need to truncate the log before persisting unstable.entries.
 pub(crate) struct Unstable {
     // the incoming unstable snapshot, if any.
     snapshot: Option<Snapshot>,
@@ -95,24 +95,114 @@ impl Unstable {
         self.snapshot = Some(s);
     }
 
-    pub(crate) fn truncate_and_append(&mut self, ents: Vec<Entry>) {
+    pub(crate) fn truncate_and_append(&mut self, ents: &[Entry]) {
         match ents[0].get_Index() {
             after if after == self.offset + self.entries.len() as u64 => {
                 // after is the next index in the self.entries
                 // directly append
-                self.entries.extend_from_slice(ents.as_slice());
+                self.entries.extend_from_slice(ents);
             }
             after if after <= self.offset => {
                 info!("replace the unstable entries from index {}", after);
                 // The log is being truncated to before our current offset
                 // portion, so set the offset and replace the entries
                 self.offset = after;
-                self.entries = ents;
+                self.entries.clear();
+                self.entries.extend_from_slice(ents);
             }
             after => {
-                // truncate to after
-                // TODO
+                // truncate to after and copy to self.entries
+                // then append
+                info!("truncate the unstable entries before index {}", after);
+                self.entries.truncate((after - self.offset) as usize);
+                self.entries.extend_from_slice(&ents);
             }
         }
+    }
+
+    fn slice(&self, lo: u64, hi: u64) -> Vec<Entry> {
+        self.must_check_out_of_bounds(lo, hi);
+        self.entries[(lo - self.offset) as usize..(hi - self.offset) as usize].to_vec()
+    }
+
+    // self.offset <= lo <= hi <= self.offset + self.entries.len()
+    fn must_check_out_of_bounds(&self, lo: u64, hi: u64) {
+        if lo > hi {
+            panic!("invalid unstable.slice {} > {}", lo, hi);
+        }
+        let upper = self.offset + self.entries.len() as u64;
+        if lo < self.offset || hi > upper {
+            panic!("unstable.slice[{}, {}] out of bound [{}, {}]", lo, hi, self.offset, upper);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::raft::raftpb::raft::{Snapshot, Entry};
+    use crate::raft::unstable::Unstable;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn it_unstable_maybe_first_index() {
+        // (entries, offset, snapshot, w_ok, w_index)
+        let tests = vec![
+            // no snapshot
+            (vec![new_entry(5, 1)], 0, None, false, 0), (vec![], 0, None, false, 0),
+            // has snapshot
+            (vec![new_entry(5, 1)], 5, Some(new_snapshot(4, 1)), true, 5), (vec![], 5, Some(new_snapshot(4, 1)), true, 5)];
+        for (i, (entries, offset, snapshot, w_ok, w_index)) in tests.iter().enumerate() {
+            let mut u = Unstable {
+                snapshot: snapshot.clone(),
+                entries: entries.clone(),
+                offset: *offset,
+            };
+            match u.maybe_first_index() {
+                Some(i) => {
+                    assert_eq!(i, *w_index);
+                }
+                None => assert!(!*w_ok),
+            }
+        }
+    }
+
+    #[test]
+    fn it_maybe_last_index() {
+        // (entries, offset, snapshot, w_ok, w_index)
+        let tests = vec![
+            // last in entries
+            (vec![new_entry(5, 1)], 5, None, true, 5),
+        ];
+        for (i, (entries, offset, snapshot, w_ok, w_index)) in tests.iter().enumerate() {
+            let mut u = Unstable {
+                snapshot: snapshot.clone(),
+                entries: entries.clone(),
+                offset: *offset,
+            };
+            match u.maybe_last_index() {
+                Some(i) => {
+                    assert_eq!(i, *w_index);
+                }
+                None => assert!(!*w_ok),
+            }
+        }
+    }
+
+    fn new_entry(index: u64, term: u64) -> Entry {
+        let mut entry = Entry::new();
+        entry.set_Term(term);
+        entry.set_Index(index);
+        entry
+    }
+
+    fn new_snapshot(index: u64, term: u64) -> Snapshot {
+        let mut snapshot = Snapshot::new();
+        snapshot.mut_metadata().set_index(index);
+        snapshot.mut_metadata().set_term(term);
+        snapshot
     }
 }
